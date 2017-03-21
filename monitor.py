50c4797b0ea8d00639ff126d97532e5630b2d84c
bcochran <b.cochrane@imperial.ac.uk>
2017-03-21 16:30:34 +0000

    webgui

--- monitor.py
+++ monitor.py
@@ -34,15 +34,6 @@ def display_data():
     conn.close()
 
 
-#tfile = open("/sys/bus/w1/devices/10-00080225f15a/w1_slave") 
-#text = tfile.read() 
-#tfile.close() 
-#secondline = text.split("\n")[1] 
-#temperaturedata = secondline.split(" ")[9] 
-#temperature = float(temperaturedata[2:]) 
-#temperature = temperature / 1000 
-#print temperature
-
 def get_temp(devicefile):
 
     try:
